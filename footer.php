d4bb7dfc04e50e39648433c89e30e5d27fec78e4
Morr-valir <picard.matthieu1@gmail.com>
2021-03-04 15:10:11 +0400

    vuejs dev active

--- footer.php
+++ footer.php
@@ -17,7 +17,8 @@
       <script src="<?php echo get_template_directory_uri(); ?>/assets/burger.js" ></script>
       <!--Import Vuejs-->
       <script src="https://unpkg.com/axios/dist/axios.min.js" ></script>
-      <script src="https://cdn.jsdelivr.net/npm/vue@2.6.0" ></script>
+      <!-- <script src="https://cdn.jsdelivr.net/npm/vue@2.6.0" ></script> -->
+      <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
       <script src="<?php echo get_template_directory_uri(); ?>/assets/Vue/composant.js"></script>
       <script src="<?php echo get_template_directory_uri(); ?>/assets/darkmode.js"></script>
     </body>
