6b737ee1b56ff2be2d3b0224f06165a239ef51e9
Morr-valir <picard.matthieu1@gmail.com>
2021-03-16 15:30:33 +0400

    correction html

--- templates/page-annuaire.php
+++ templates/page-annuaire.php
@@ -8,21 +8,19 @@ get_header(); ?>
             <label>Recherche par Nom</label>
             <input class="input" type="text" v-model="liste" style="margin-bottom:25px;">
             <div class="columns is-multiline" style="padding: 20px ;justify-content: center;">
-            <div class="column is-3 box" style="margin: 10px;" v-for="(list,i) in filteredList" :key="i" :key_contact="list.id" :nom="list.nom" :service="list.pole" :type="list.type" :num="list.tel">
-              <div class="media-content">
-                <div class="content" style="position: relative;">
-                  <p>
-                    <strong>{{list.nom}}</strong> <small class="tag type"> Numéro : {{list.type}}</small>
-                    <br>
-                    <br>
-                    <strong>{{list.tel}}</strong>
-                  </p>
+                <div class="column is-3 box" style="margin: 10px;" v-for="(list,i) in filteredList" :key="i" :key_contact="list.id" :nom="list.nom" :service="list.pole" :type="list.type" :num="list.tel">
+                    <div class="columns">
+                        <div class="column is-7">
+                            <strong>{{list.nom}}</strong>
+                            <br>
+                            <strong>{{list.tel}}</strong>
+                        </div>
+                        <div class="column">
+                            <small class="tag type"> Numéro : {{list.type}}</small>
+                        </div>
+                    </div>
                 </div>
-            </div>
-
           </div>
-
-        </div>
         </section>
     </main>
 
