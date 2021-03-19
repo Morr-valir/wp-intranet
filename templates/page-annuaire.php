81b65bb2c2bd7924b65342a2d4b0c36dc18ad50f
Morr-valir <picard.matthieu1@gmail.com>
2021-03-19 10:15:25 +0400

    night mode carte

--- templates/page-annuaire.php
+++ templates/page-annuaire.php
@@ -8,17 +8,12 @@ get_header(); ?>
             <label>Recherche par Nom</label>
             <input class="input" type="text" v-model="liste" style="margin-bottom:25px;">
             <div class="columns is-multiline" style="padding: 20px ;justify-content: center;">
-                <div class="column is-3 box" style="margin: 10px;" v-for="(list,i) in filteredList" :key="i" :key_contact="list.id" :nom="list.nom" :service="list.pole" :type="list.type" :num="list.tel">
-                    <div class="columns">
-                        <div class="column is-7">
-                            <strong>{{list.nom}}</strong>
-                            <br>
-                            <strong>{{list.tel}}</strong>
-                        </div>
-                        <div class="column">
-                            <small class="tag type"> Numéro : {{list.type}}</small>
-                        </div>
-                    </div>
+                <div class=" box column is-3" style="margin: 10px;" v-for="(list,i) in filteredList" :key="i" :key_contact="list.id" :nom="list.nom" :service="list.pole" :type="list.type" :num="list.tel">
+                    <!--NEW CARTE-->
+                    <header class="carte-header">
+                        <p>{{list.nom}}<br>{{list.tel}}</p>
+                        <small class="tag">Numéro : {{list.type}}</small>
+                    </header>
                 </div>
           </div>
         </section>
